290a4a2f6d4d30dcf47fceacdb4c820cc7a4c9d4
Daniel Walker <walkerda2010@gmail.com>
2014-10-03 02:29:14 +0000

    Second commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name toeicTimerApp
+ * @description
+ * # toeicTimerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('toeicTimerApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name toeicTimerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the toeicTimerApp
+ */
+angular.module('toeicTimerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
