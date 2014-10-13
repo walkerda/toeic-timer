ff12e01858325937e4eb0825b831733ce4731b0e
Daniel Walker <walkerda2010@gmail.com>
2014-10-13 08:20:43 +0000

    added reset functionality and timer duration by minutes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,22 +22,22 @@ function PartsCtrl($scope) {
             name: 'part5',
             questionNumbers: [101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121,
                 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140],
-            timerDuration: 15*60
-            //questionDuration: d.setSeconds(23)
+            timerDuration: 15 * 60,
+            questionDuration: 23 * 1000
         },
         {
             name: 'part6',
             questionNumbers: [141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152],
-            timerDuration: 6*60
-            //questionDuration: d.setSeconds(32)
+            timerDuration: 6 * 60,
+            questionDuration: 32 * 1000
         },
         {
             name: 'part7',
             questionNumbers: [153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173,
                 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197,
                 198, 199, 200],
-            timerDuration: 54*60
-            //questionDuration: d.setSeconds(68)
+            timerDuration: 54 * 60,
+            questionDuration: 68 * 1000
         },
         {
             name: 'allparts',
@@ -46,8 +46,8 @@ function PartsCtrl($scope) {
                 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169,
                 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193,
                 194, 195, 196, 197, 198, 199, 200],
-            timerDuration: 75*60
-            //questionDuration: d.setSeconds(45)
+            timerDuration: 75 * 60,
+            questionDuration: 45 * 1000
         }
     ];
 
@@ -125,6 +125,8 @@ function TimerCtrl($scope) {
     $scope.startTimer = function (){
         $scope.$broadcast('timer-start');
         $scope.timerRunning = true;
+        document.getElementById("startBTN").innerHTML = "RESUME";
+        //document.getElementById("timercountdown").innerHTML = PartsCtrl.activePart.timerDuration;
     };
 
     $scope.stopTimer = function (){
@@ -132,7 +134,11 @@ function TimerCtrl($scope) {
         $scope.timerRunning = false;
     };
 
-    //$scope.countdownVal = PartsCtrl.activePart.timerDuration;
+    $scope.resetTimer = function (){
+        $scope.$broadcast('timer-reset');
+        $scope.timerRunning = false;
+        document.getElementById("startBTN").innerHTML = "START";
+    };
 
     TimerCtrl.$inject = ['$scope'];
 }
