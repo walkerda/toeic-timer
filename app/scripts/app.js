eea7583d2849f3518137d87d3a4fdabc8a4446a0
Daniel Walker <walkerda2010@gmail.com>
2014-10-14 11:29:45 +0000

    fixed controller inheritance, setup navbar buttons, incorporated timerbar

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,14 +9,14 @@
  * Main module of the application.
  */
 
+// PartsCtrl function that's passed into the PartsCtrl
 function PartsCtrl($scope) {
-    //var d = new Date();
     $scope.parts = [
         {
             name: 'landing',
             questionNumbers: ['---'],
-            timerDuration: 0
-            //questionDuration: d.setSeconds(0)
+            timerDuration: 0,
+            questionDuration: 0
         },
         {
             name: 'part5',
@@ -64,69 +64,14 @@ function PartsCtrl($scope) {
 }
 
 
-//function PartsService($rootScope) {
-//    var d = new Date();
-//    $rootScope.parts = [
-//        {
-//            name: 'landing',
-//            questionNumbers: ['---'],
-//            timerDuration: d.setMinutes(0),
-//            questionDuration: ''
-//        },
-//        {
-//            name: 'part5',
-//            questionNumbers: [101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121,
-//                122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140],
-//            timerDuration: d.setMinutes(15,0),
-//            questionDuration: d.setSeconds(23,8)
-//        },
-//        {
-//            name: 'part6',
-//            questionNumbers: [141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152],
-//            timerDuration: d.setMinutes(6,0),
-//            questionDuration: d.setSeconds(32,7)
-//        },
-//        {
-//            name: 'part7',
-//            questionNumbers: [153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173,
-//                174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197,
-//                198, 199, 200],
-//            timerDuration: d.setMinutes(54,0),
-//            questionDuration: d.setSeconds(68,9)
-//        },
-//        {
-//            name: 'allparts',
-//            questionNumbers: [101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121,
-//                122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145,
-//                146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169,
-//                170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193,
-//                194, 195, 196, 197, 198, 199, 200],
-//            timerDuration: d.setMinutes(75,0),
-//            questionDuration: d.setSeconds(45,5)
-//        }
-//    ];
-//
-//    $rootScope.activePart = $rootScope.parts[0];
-//
-//    return {
-//        setPart: function(index) {
-//            $rootScope.activePart = $rootScope.parts[index];
-//        },
-//        isSet: function(index) {
-//            return $rootScope.activePart === $rootScope.parts[index];
-//        }
-//
-//    };
-//}
-
+// TimerCtrl function that's passed into the TimerCtrl
 function TimerCtrl($scope) {
     $scope.timerRunning = false;
+    $scope.startLabel = "START";
 
     $scope.startTimer = function (){
         $scope.$broadcast('timer-start');
         $scope.timerRunning = true;
-        document.getElementById("startBTN").innerHTML = "RESUME";
-        //document.getElementById("timercountdown").innerHTML = PartsCtrl.activePart.timerDuration;
     };
 
     $scope.stopTimer = function (){
@@ -137,24 +82,11 @@ function TimerCtrl($scope) {
     $scope.resetTimer = function (){
         $scope.$broadcast('timer-reset');
         $scope.timerRunning = false;
-        document.getElementById("startBTN").innerHTML = "START";
     };
 
-    TimerCtrl.$inject = ['$scope'];
 }
 
-
-
-
-
-//function ConsoleLogger() {
-//    return {
-//        test: function() {
-//            console.log("IT WORKS!!!");
-//        }
-//    };
-//}
-
+// angular app setup
 angular
     .module('toeicTimerApp', ['timer'])
     .controller('PartsCtrl', PartsCtrl)
