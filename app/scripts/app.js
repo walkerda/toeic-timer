c7980d23ab62b1bc0321506387730f95f5177962
Daniel Walker <walkerda2010@gmail.com>
2014-10-16 09:06:14 +0000

    enabled timerDuration to be visible after selecting an activePart from the menu, and when resetting the timer

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,20 +19,20 @@ function PartsCtrl($scope) {
             questionDuration: 0
         },
         {
-            name: 'part5',
+            name: 'part 5',
             questionNumbers: [101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121,
                 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140],
             timerDuration: 15 * 60,
             questionDuration: 39
         },
         {
-            name: 'part6',
+            name: 'part 6',
             questionNumbers: [141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152],
             timerDuration: 6 * 60,
             questionDuration: 54
         },
         {
-            name: 'part7',
+            name: 'part 7',
             questionNumbers: [153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173,
                 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197,
                 198, 199, 200],
@@ -40,7 +40,7 @@ function PartsCtrl($scope) {
             questionDuration: 75
         },
         {
-            name: 'allparts',
+            name: 'all parts',
             questionNumbers: [101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121,
                 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145,
                 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169,
@@ -82,22 +82,34 @@ function PartsCtrl($scope) {
 // TimerCtrl function that's passed into the TimerCtrl
 function TimerCtrl($scope) {
     $scope.timerRunning = false;
-    $scope.startLabel = "START";
+    $scope.timerPaused = false;
 
     $scope.startTimer = function (){
         $scope.$broadcast('timer-start');
         $scope.timerRunning = true;
+        $scope.timerPaused = false;
     };
 
     $scope.stopTimer = function (){
         $scope.$broadcast('timer-stop');
         $scope.timerRunning = false;
+        $scope.timerPaused = true;
     };
 
     $scope.resetTimer = function (){
         $scope.$broadcast('timer-reset');
         $scope.timerRunning = false;
+        $scope.timerPaused = false;
     };
+
+    $scope.timerResume = function (){
+        $scope.$broadcast('timer-resume');
+    };
+
+    $scope.clearTimer = function (){
+        $scope.$broadcast('timer-clear');
+        $scope.timerRunning = false;
+    }
 
 }
 
